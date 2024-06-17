af6e830e07c28b96e181898f6359f5f1e21889ea
Lisowez <lisowez@icloud.com>
2024-06-17 17:11:57 +0300

    feat: done homework 5

--- homeworks/homework5.js
+++ homeworks/homework5.js
@@ -2,32 +2,32 @@
 // const a = {b: 1},
 //     c = Object.create(a);
 //
-// console.log(c.b); // ?
+// console.log(c.b); // 1
 // delete c.b;
-// console.log(c.b); // ?
+// console.log(c.b); // 1
 // delete a.b;
-// console.log(c.b); // ?
+// console.log(c.b); // undefined
 // a.z = 2;
-// console.log(c.z); // ?
+// console.log(c.z); // 2
 // c.z = 3;
-// console.log(a.z); // ?
+// console.log(a.z); // 2
 
 // 2.
 
 // const promise = new Promise(() => {
 // })
-// promise.prototype === Promise.__proto__ // ?
+// promise.prototype === Promise.__proto__ // false
 //
 // const obj = {}
-// obj.__proto__ === Object.prototype // ?
+// obj.__proto__ === Object.prototype // true
 //
-// new Array([]).__proto__ === Array.prototype // ?
+// new Array([]).__proto__ === Array.prototype // true
 //
 // function Fn1 () {}
 // function Fn2 () {}
-// Fn1.constructor === Fn2.constructor // ?
+// Fn1.constructor === Fn2.constructor // true
 //
-// Fn1.prototype === Fn2.prototype // ?
+// Fn1.prototype === Fn2.prototype // false
 //3.
 
 // У вас есть два конструктора, Animal и Bird.
@@ -48,5 +48,25 @@
 // animal.speak(); // "Some generic sound"
 // bird.speak();   // "Some generic sound"
 // bird.fly();     // "Flying high!"
+function Animal(name) {
+  this.name = name;
+}
 
+Animal.prototype.speak = function () {
+  console.log(`Some generic sound`);
+};
 
+function Bird(name) {
+  Animal.call(this, name);
+}
+
+Bird.prototype = Object.create(Animal.prototype);
+
+Bird.prototype.constructor = Bird;
+
+Bird.prototype.fly = function () {
+  console.log(`Flying high!`);
+};
+
+const animal = new Animal("Дженни");
+const bird = new Bird("Воробей");
